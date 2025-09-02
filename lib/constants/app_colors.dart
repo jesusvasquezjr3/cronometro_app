5fa0a650367ebb9337301a8f23f365f45841ce8f
Jesús Vásquez <182829189+jesusvasquezjr3@users.noreply.github.com>
2025-09-01 18:52:45 -0600

    Create app_colors.dart

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backgroundColor = Color(0xFF1A1A2E);
+  static const Color cardBackground = Color(0xFF16213E);
+  static const Color primaryBlue = Color(0xFF0F3460);
+  static const Color accentOrange = Color(0xFFE94560);
+  static const Color textPrimary = Color(0xFFFFFFFF);
+  static const Color textSecondary = Color(0xFFB0BEC5);
+  
+  static const Color startButtonColor = Color(0xFF4CAF50);
+  static const Color pauseButtonColor = Color(0xFFFF9800);
+  static const Color resetButtonColor = Color(0xFFF44336);
+  
+  static const Color buttonDisabled = Color(0xFF757575);
+}
